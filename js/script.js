974f90c0674ad496db03bdea0d2c644941b9eb85
Jason Duquain <jduquain@gmail.com>
2019-07-07 22:02:21 -0400

    added nav submenu dropdowns and hero slider

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+/***********  TESTIMONIAL SLIDER  ***********/
+
+$(document).ready(function(){
+  $('.hero').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+  });
+});
