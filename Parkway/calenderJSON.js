3918470c0a27dbd01973e81ebb429fec563267c0
Jason Duquain <jason.duquain@gmail.com>
2019-11-04 13:31:51 -0500

    updated remaining calender events

---
+++ Parkway/calenderJSON.js
@@ -0,0 +1,171 @@
+events: [
+            {
+              title: 'Turn back Clocks',
+              start: '2019-11-03'
+            },
+            {
+              title: 'In house field trip',
+              start: '2019-11-04',
+              end: '2019-11-05'
+            },
+            {
+              title: 'In honor of veterans day wear red, white, and blue',
+              start: '2019-11-11'
+            },
+            {
+              title: 'Thanksgiving feasts',
+              start: '2019-11-21',
+              end: '2019-11-22',
+                
+            },
+            {
+              title: 'Thanksgiving break (NO SCHOOL)',
+              start: '2019-11-25',
+              end: '2019-11-29'
+            },
+            {
+              title: 'Christmas Program',
+              start: '2019-12-05T10:30:00',
+              end: '2019-12-06'
+            },
+            {
+              title: 'Birthday Parties for Jesus',
+              start: '2019-12-05',
+              end: '2019-12-06'
+            },
+            {
+              title: 'Christmas Break (NO SCHOOL)',
+              start: '2019-12-18',
+              end: '2020-01-07'
+            },
+            {
+              title: 'FIRST DAY BACK IN 2020!',
+              start: '2020-01-08'
+            }, 
+            {
+              title: 'Tour Days (1:15pm)',
+              start: '2020-01-13T13:15:00'
+            },
+            {
+              title: 'Tour Days (1:15pm)',
+              start: '2020-01-14T13:15:00'
+            },
+            {
+              title: 'MLK (NO SCHOOL)',
+              start: '2020-01-20'
+            },
+            {
+              title: '2020-21 Registration Begins',
+              start: '2020-01-21'
+            },
+            {
+              title: 'Donuts for Dads 8:30am',
+              start: '2020-01-30T08:30:00',
+              end: '2020-01-301T09:30:00'
+            },
+            {
+              title: 'Donuts for Dads 8:30am',
+              start: '2020-01-31T08:30:00',
+              end: '2020-01-31T09:30:00'
+            },
+            {
+              title: 'Dental Visit',
+              start: '2020-02-03',
+              end: '2020-02-04'
+            },
+            {
+              title: 'Valentine’s Day Class Celebrations',
+              start: '2020-02-12',
+              end: '2020-02-13'
+            },
+            {
+              title: 'NO SCHOOL',
+              start: '2020-02-14',
+              end: '2020-02-18'
+            },
+            {
+              title: 'Cap and Gown Pictures (4s & 5s)',
+              start: '2020-02-26'
+            },
+            {
+              title: 'Spring Pictures',
+              start: '2020-02-27'
+            },
+            {
+              title: 'Spring Pictures',
+              start: '2020-02-28'
+            },
+            {
+              title: 'Muffins for Mom (End of Day)',
+              start: '2020-03-05'
+            },
+            {
+              title: 'Muffins for Mom (End of Day)',
+              start: '2020-03-06'
+            },
+            {
+              title: 'Spring Forward',
+              start: '2020-03-08'
+            },
+            {
+              title: 'NO SCHOOL',
+              start: '2020-03-12'
+            },
+            {
+              title: 'NO SCHOOL',
+              start: '2020-03-13'
+            },
+            {
+              title: 'Mother Goose',
+              start: '2020-03-19'
+            },
+            {
+              title: 'Mother Goose',
+              start: '2020-03-20'
+            },
+            {
+              title: 'Teacher Appreciation',
+              start: '2020-03-26'
+            },
+            {
+              title: 'Teacher Appreciation',
+              start: '2020-03-27'
+            },
+            {
+              title: 'Easter Celebrations',
+              start: '2020-04-02'
+            },
+             {
+              title: 'Easter Celebrations',
+              start: '2020-04-03'
+            },
+            {
+              title: 'NO SCHOOL (spring Break)',
+              start: '2020-04-06',
+              end: '2020-04-10'
+            },
+            {
+              title: 'Bus Visit 10 am (4s & 5s)',
+              start: '2020-04-30'
+            },
+            {
+              title: 'Fun in the Sun',
+              start: '2020-05-07'
+            },
+            {
+              title: 'Fun in the Sun',
+              start: '2020-05-08'
+            },
+            {
+              title: 'End of Year Program and last day (2s & 3s)',
+              start: '2020-05-12T10:30:00'
+            },
+              {
+              title: 'End of Year Program and last day (2s & 3s)',
+              start: '2020-05-13T10:30:00'
+            },
+            {
+              title: 'End of Year Program and Graduation (4s & 5s)',
+              start: '2020-05-14T10:30:00'
+            }
+          ]
